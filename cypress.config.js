2bac35a5511b4143175f10f23f3d6c8b3b760ea9
Dilip K M <89456346+dilipwam@users.noreply.github.com>
2024-11-13 20:05:55 +0530

    Minor Updates - Cypress Cucumber

--- cypress.config.js
+++ cypress.config.js
@@ -1,22 +1,33 @@
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
-const createEsbuildPlugin= require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const addCucumberPreprocessorPlugin =  require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsbuildPlugin =  require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    defaultCommandTimeout: 60000,
+    pageLoadTimeout: 120000,
+    experimentalSessionAndOrigin: true,
+
+    specPattern: ["cypress/e2e/**/*.feature", "cypress/e2e/**/*.cy.js"],
+    stepDefinitions: [
+      "cypress/e2e/[filepath]/**/*.{js,ts}",
+      "cypress/e2e/[filepath].{js,ts}",
+      "cypress/support/step_definitions/**/*.{js,ts}",
+    ],
 
     setupNodeEvents(on, config) {
       const bundler = createBundler({
-        plugins:[createEsbuildPlugin(config)],
+        plugins: [createEsbuildPlugin(config)],
       });
 
-      on("file:preprocessor",bundler);
-       addCucumberPreprocessorPlugin(on, config);
+      on("file:preprocessor", bundler);
+      addCucumberPreprocessorPlugin(on, config);
       return config;
-
     },
 
-    specPattern: "cypress/e2e/**/*.feature",
+   
   },
 });
--- cypress/support/step_definitions/AnyThing.js
+++ cypress/support/step_definitions/AnyThing.js
@@ -1,10 +1,10 @@
 import { When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
-When("I visit duckduckgo.com", () => {
+When("I visit duckduckgo", () => {
   cy.visit("https://www.duckduckgo.com");
 });
 
-Then("I should see a search bar", () => {
+Then("I should see a search bar for sure", () => {
   cy.get('input#searchbox_input')
   .should("be.visible")
   .should("have.attr","aria-label","Search with DuckDuckGo")
